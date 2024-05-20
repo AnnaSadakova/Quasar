2483b36ec09885111cdad01f33ad201bf5655716
AnnaSadakova <162958637+AnnaSadakova@users.noreply.github.com>
2024-05-20 18:59:20 +0300

    10.3

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,7 +1,73 @@
 <template>
-  <q-page class="q-pa-lgr">
-    <h5>ToDo</h5>
+  <q-page class="bg-blue-1 column">
+    <q-list separator bordered>
+      <q-item
+        @click="task.done = !task.done"
+        clickable
+        :class="{ 'done bg-green-1': task.done }"
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        v-ripple
+      >
+        <q-item-section avatar top>
+          <q-checkbox
+            class="no-pointer-events"
+            v-model="task.done"
+            val="cyan"
+            color="cyan"
+          />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            @click.stop="deleteTask(index)"
+            dense
+            flat
+            round
+            color="primary"
+            icon="delete"
+          />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "hello",
+          done: false,
+        },
+        {
+          title: "hello1",
+          done: false,
+        },
+        {
+          title: "hello2",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: rgb(0, 0, 0);
+  }
+}
+</style>
